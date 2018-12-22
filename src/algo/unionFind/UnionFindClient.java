2d779b9927c57957f1dae0ab9538bb934c4a9c08
Vishal Khatri <vishalak@uw.edu>
2018-12-22 13:18:28 -0800

    QuickFind Code

--- src/algo/unionFind/UnionFindClient.java
+++ src/algo/unionFind/UnionFindClient.java
@@ -1,10 +1,38 @@
 package algo.unionFind;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class UnionFindClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner stdIn = null;
+		try {
+			stdIn = new Scanner(new File("QuickFindInput.txt"));
+			int N = stdIn.nextInt();
+			UF uf = new UF(N);
+			
+			while(!stdIn.hasNext())
+			{
+				int p = stdIn.nextInt();
+				int q = stdIn.nextInt();
+				if(!uf.connected(p, q))
+				{
+					uf.union(p, q);
+					System.out.println(p + " " + q);
+				}
+			}
+			
+			stdIn.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 
 }
