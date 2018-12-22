c9faa3e0087d4689382aa649857d945da9be0c20
Vishal Khatri <vishalak@uw.edu>
2018-12-22 15:13:56 -0800

    Added Quick Union Algo files

---
+++ src/algo/unionFind/QuickUnionClient.java
@@ -0,0 +1,42 @@
+package algo.unionFind;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class QuickUnionClient {
+
+	public static void main(String[] args) {
+
+		Scanner stdIn = null;
+		try {
+			stdIn = new Scanner(new File("QuickFindInput.txt"));
+			int N = stdIn.nextInt();
+			QuickUnion quf = new QuickUnion(N);
+			
+			while(stdIn.hasNextInt())
+			{
+				int p = stdIn.nextInt();
+				int q = stdIn.nextInt();
+				if(!quf.connected(p, q))
+				{
+					quf.union(p, q);
+					System.out.println(p + " " + q + " are now connected");
+				}
+				else
+				{
+					System.out.println(p + " " + q + " already connected");
+				}
+			}
+			
+			stdIn.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
